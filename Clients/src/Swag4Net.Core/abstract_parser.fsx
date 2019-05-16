ef8dad281c567c7bc11c849f3236776a9c306830
Flechner Romain <rflechner@romcyber.com>
2019-05-16 22:24:11 +0200

    parse instructions

--- Clients/src/Swag4Net.Core/abstract_parser.fsx
+++ Clients/src/Swag4Net.Core/abstract_parser.fsx
@@ -67,21 +67,76 @@ type PathInstruction =
   | SelectOffset of int32
 and PathSelect = PathInstruction list
 
-let parsePath (path:string) : PathSelect =
-  let rec loop acc chars =
-    let m = chars |> Array.takeWhile (fun c -> Char.IsLetterOrDigit c || c = '_' || c = '-')
-    let p = System.String m
-    let r = (SelectMember p) :: acc
-    if chars.Length > m.Length
-    then
-      //let sep = chars.[m.Length]
-      chars |> Array.skip (m.Length + 1) |> loop r
-    else
-      r
-  path.ToCharArray() |> loop [] |> List.rev
+let (|ReadWhile|_|) f (chars:char list) =
+  let m = chars |> List.takeWhile f
+  if m |> List.isEmpty
+  then None
+  else
+    let n = min m.Length (chars.Length)
+    let r = chars |> List.skip n
+    let txt = m |> Seq.toArray |> System.String
+    Some (txt, r)
 
-parsePath "popo"
-parsePath "popo.tata"
+let (|ReadTo|_|) (search:char list) (chars:char list) =
+  let f i = search |> List.contains i |> not
+  (|ReadWhile|_|) f (chars:char list)
 
-//[|'d';'r'|] |> System.String
+let betweenChars a b (buffer:char list) =
+  if buffer.Length <= 0 || buffer.Head <> a
+  then None
+  else
+    match buffer |> List.skip 1 |> List.tryFindIndex (fun c -> c = b) with
+    | None -> None
+    | Some i ->
+        let result = 
+          buffer
+          |> List.skip 1
+          |> List.take i
+        let rest = buffer |> List.skip (result.Length + 2)
+        Some(result, rest)
+
+let (|BetweenChars|_|) a b (buffer:char list) =
+  betweenChars a b buffer
+
+let parseInstructions (path:string) =
+  let rec loop acc text =
+    match text with
+    | ReadTo ['.'; '['; ']'] (name, r) -> 
+        loop ((SelectMember (string name)) :: acc) r
+    | '.' :: r ->
+        loop acc r
+    | BetweenChars '[' ']' (nums, r) ->
+        if nums |> Seq.exists (Char.IsDigit >> not) 
+        then Error "offset can be only digit"
+        else
+          let o = nums |> List.toArray |> System.String |> string |> Int32.Parse
+          loop ((SelectOffset o) :: acc) r
+    | [] -> acc |> List.rev |> Ok
+    | r -> 
+      let o = path.Length - r.Length
+      Error <| sprintf "Invalid syntax '%c' at char %d" r.Head o
+  path |> Seq.toList |> loop []
+
+parseInstructions "popo.lala.mamam[0].ages[35]"
+parseInstructions "lklk"
+parseInstructions "popo.lala.mamam[0dz].ages[35]"
+
+o
+
+// let parsePath (path:string) : PathSelect =
+//   let rec loop acc chars =
+//     let m = chars |> Array.takeWhile (fun c -> Char.IsLetterOrDigit c || c = '_' || c = '-')
+//     let p = System.String m
+//     let r = (SelectMember p) :: acc
+//     if chars.Length > m.Length
+//     then
+//       //let sep = chars.[m.Length]
+//       chars |> Array.skip (m.Length + 1) |> loop r
+//     else
+//       r
+//   path.ToCharArray() |> loop [] |> List.rev
+
+// parsePath "popo"
+// parsePath "popo.tata"
+
 
