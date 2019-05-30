c219710d81ee90ce4139fe055a991be5983a4b29
Flechner Romain <rflechner@romcyber.com>
2019-05-30 18:03:07 +0200

    refactoring

--- Clients/src/Swag4Net.Core/Validator.fs
+++ Clients/src/Swag4Net.Core/Validator.fs
@@ -6,11 +6,13 @@ open Newtonsoft.Json.Linq
 open Newtonsoft.Json.Schema
 open System.Reflection
 
+#if !INTERACTIVE
 let internal ReadEmbedded(path: string) =
     Assembly.GetExecutingAssembly().GetManifestResourceStream(path)
+#endif
 
-let validateV3 (content:string) =
-    use s = new StreamReader(ReadEmbedded("Swag4Net.Core.v3.openapi-jsonschema.json"))
+let validateV3' (stream:Stream) (content:string) =
+    use s = new StreamReader(stream)
     use reader = new JsonTextReader(s)
     let schema = JSchema.Load(reader)
     let json = JObject.Parse content
@@ -20,3 +22,8 @@ let validateV3 (content:string) =
         Error(!errors)
     else
         Ok()
+
+#if !INTERACTIVE
+let validateV3 =
+  "Swag4Net.Core.v3.openapi-jsonschema.json" |> ReadEmbedded |> validateV3'
+#endif
--- Clients/src/Swag4Net.Core/v3/JsonParser.fs
+++ Clients/src/Swag4Net.Core/v3/JsonParser.fs
@@ -5,23 +5,32 @@ open System
 open Swag4Net.Core.v3.SpecificationDocument
 open Newtonsoft.Json
 
-exception ParsingErrorException of string
 
 let internal getTokenOrThrow (token:JToken) (tag: string) =
-    token.SelectToken tag |> fun t -> if isNull t then raise (ParsingErrorException ((sprintf "'%s': field is missing" tag))) else t
+    token.SelectToken tag |> fun t -> if isNull t then Error (sprintf "'%s': field is missing" tag) else Ok t
 
 let internal getOptionalToken (cb: JToken -> 'r) (token:JToken) (tag: string) =
     token.SelectToken tag |> fun t -> if isNull t then None else Some (cb t)
     
 let internal parseInfos (spec:JToken) =
   let infoToken = spec.SelectToken "info"
-  if isNull infoToken then raise (ParsingErrorException ("no info field provided"))
-  { Description=    getOptionalToken (fun t -> string t) infoToken "description"
-    Version=        getTokenOrThrow infoToken "version" |> string
-    Title=          getTokenOrThrow infoToken "title" |> string
-    TermsOfService= getOptionalToken (fun t -> string t) infoToken  "termsOfService"
-    Contact=        getOptionalToken (fun t -> Email (string t)) infoToken "contact.email"
-    License=        getOptionalToken (fun t -> t.ToObject<License>()) infoToken "license" }
+  if isNull infoToken
+  then Error "no info field provided"
+  else
+      getTokenOrThrow infoToken "version"
+        |> Result.bind(
+              fun version ->
+                getTokenOrThrow infoToken "title"
+                  |> Result.bind(
+                      fun title ->
+                        Ok { Description=    getOptionalToken (fun t -> string t) infoToken "description"
+                             Version=        version |> string
+                             Title=          title |> string
+                             TermsOfService= getOptionalToken (fun t -> string t) infoToken  "termsOfService"
+                             Contact=        getOptionalToken (fun t -> Email (string t)) infoToken "contact.email"
+                             License=        getOptionalToken (fun t -> t.ToObject<License>()) infoToken "license" }
+                     )
+            )
 
 let internal parseStandard (jsonSpec:JToken) =
     {
@@ -31,17 +40,20 @@ let internal parseStandard (jsonSpec:JToken) =
 let parse (content:string) =
     try
         let jsonSpec = JObject.Parse content
-        Ok { 
-            Standard= parseStandard jsonSpec
-            Infos= parseInfos jsonSpec
-            Servers=None
-            Paths=Map.empty
-            Components=None
-            Security=None
-            Tags=None
-            ExternalDocs=None }
+        jsonSpec
+        |> parseInfos
+        |> Result.map (fun infos -> 
+            { 
+              Standard= parseStandard jsonSpec
+              Infos= infos
+              Servers=None
+              Paths=Map.empty
+              Components=None
+              Security=None
+              Tags=None
+              ExternalDocs=None
+            })
 
     with
         | :? ArgumentNullException -> Error "provided specification content is empty"
         | :? JsonReaderException as ex -> Error (sprintf "provided specification is not valid json: %s" ex.Message)
-        | ParsingErrorException(msg) -> Error (sprintf "parsing failed: %s" msg)
